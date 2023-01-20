c7d648d85f9707060aa8096834711506091984c1
Kailash Dhungana <dhunganak1920@gmail.com>
2023-01-20 20:12:51 +0545

    remove tailwind cdn and add tailwind through npm add responsiveness

--- src/components/home/ButtonGroup.js
+++ src/components/home/ButtonGroup.js
@@ -4,7 +4,7 @@ import StartButton from './StartButton'
 
 const ButtonGroup = () => {
   return (
-    <div className='mt-4 justify-center sm:justify-start flex gap-10'>
+    <div className='mt-4 home-btn items-center justify-center sm:justify-start flex gap-10'>
         <StartButton />
         <SeeVideo />
     </div>
